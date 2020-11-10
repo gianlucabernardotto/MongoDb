e7981b3ead61548ae6f326b9793b191949b5e849
gianlucabernardotto <bernardotto.gianluca@iisgalvanimi.edu.it>
2020-11-10 10:51:06 +0000

    5.3 v2

--- ESEMPIO_MONGO/routes/movies.js
+++ ESEMPIO_MONGO/routes/movies.js
@@ -38,5 +38,42 @@ router.get('/list/:num', function (req, res, next) {
 
     });
 });
+
+router.get('/movie_from_year/:year', function (req, res, next) {
+   
+    console.log(req.params); //Leggo i parametri passati all'url
+    year =  parseInt(req.params.year);
+    const uri = "mongodb+srv://Giangy:Gianluca@cluster0.mpo20.mongodb.net/test"
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
+        // eseguo una find sulla collection
+        collection.find({'year': year}).toArray((err, result) => {
+            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+            else res.send(result);
+            client.close(); //Quando ho terminato la find chiudo la sessione con il db
+        }); //Eseguo la query e passo una funzione di callback
+
+    });
+});
+
+router.get('/movie_from_rating/:rating', function (req, res, next) {
+   
+    console.log(req.params); //Leggo i parametri passati all'url
+    rating =  parseInt(req.params.rating);
+    const uri = "mongodb+srv://Giangy:Gianluca@cluster0.mpo20.mongodb.net/test"
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
+        // eseguo una find sulla collection
+        collection.find({'imdb.rating': rating}).toArray((err, result) => {
+            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+            else res.send(result);
+            client.close(); //Quando ho terminato la find chiudo la sessione con il db
+        }); //Eseguo la query e passo una funzione di callback
+
+    });
+});  
+
 module.exports = router;
 
